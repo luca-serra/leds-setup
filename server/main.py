07f06bdcc2422f5595a2b7828bfe84c3f1c6b643
Luca Serra <luca.serra@hotmail.fr>
2023-03-16 18:10:52 +0100

    fix: running program http now possible

--- server/main.py
+++ server/main.py
@@ -1,17 +1,25 @@
+import os
+
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Wordd"}
+    return {"message": "Welcome"}
+
+
+@app.get("/pr")
+async def programs():
+    programs = [file.replace(".py", "") for file in os.listdir("./leds/") if file.endswith(".py")]
+    return {"programs": programs}
 
 
 @app.get("/programs/{program_name}")
-async def get_program(program_name: str):
-    exec(f"from leds.{program_name} import main")
-    # TODO: run this in a separate thread
-    exec("main()")
+async def run_program(program_name: str):
+    # TODO: run this in a separate thread?
+    os.system(f"sudo python3 leds/{program_name}.py")
 
     return {"Program started": program_name}
