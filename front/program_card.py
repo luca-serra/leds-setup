821d4d099a53b2778208567a77baff304b43a866
Luca Serra <luca.serra@hotmail.fr>
2023-03-22 15:11:51 +0100

    enh: use friendly names to display programs

--- front/program_card.py
+++ front/program_card.py
@@ -2,6 +2,8 @@ import os
 import requests
 import streamlit as st
 
+from front.utils import get_friendly_program_name
+
 
 def build_program_card(program_name: str) -> None:
     def _on_click_program_card() -> None:
@@ -9,4 +11,5 @@ def build_program_card(program_name: str) -> None:
             f"{os.environ.get('HOST')}:{os.environ.get('SERVER_PORT')}/programs/{program_name}"
         )
 
-    st.button(program_name, key=program_name, on_click=_on_click_program_card)
+    program_name_display = get_friendly_program_name(program_name)
+    st.button(program_name_display, key=program_name, on_click=_on_click_program_card)
--- front/utils.py
+++ front/utils.py
@@ -1,11 +1,16 @@
+from typing import List
 import requests
 import os
 
 
-def get_server_programs():
+def get_server_programs() -> List[str]:
     program_names = (
         requests.get(f"{os.environ.get('HOST')}:{os.environ.get('SERVER_PORT')}/programs")
         .json()
         .get("programs")
     )
     return program_names
+
+
+def get_friendly_program_name(program_name: str) -> str:
+    return program_name.replace("_", " ").capitalize()
