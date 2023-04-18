694aef72b2456440c625f6d04659e2cc4fbec056
Luca Serra <luca.serra@hotmail.fr>
2023-04-18 21:05:47 +0200

    enh: Add picture to app

--- front/app.py
+++ front/app.py
@@ -6,11 +6,17 @@ from front.utils import get_server_programs
 
 load_dotenv()
 
+from PIL import Image
+
+img = Image.open("resources/photo.jpeg")
 
 st.markdown(
-    """<p align="center" style="color:black;background:linear-gradient(to bottom, #FCD8F9 0%, #FFFFFF 100%);">Bienvenue chez ChaFrench et ABP !
-    <br> Voici la liste des programmes disponibles sur le serveur :
-    </p>""",
+    """<p align="center" style="color:black;background:linear-gradient(to bottom, #FCD8F9 0%, #FFFFFF 100%);">Bienvenue chez ChaFrench et ABP !</p>""",
+    unsafe_allow_html=True,
+)
+st.image(img, use_column_width=True)
+st.markdown(
+    """<p align="center" style="color:black;background:linear-gradient(to bottom, #FCD8F9 0%, #FFFFFF 100%);">Voici la liste des programmes disponibles sur le serveur :</p>""",
     unsafe_allow_html=True,
 )
 st.markdown(
